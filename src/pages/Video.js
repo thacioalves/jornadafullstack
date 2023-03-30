a240ded32226602418b89a044cd3930b8243db93
Thacio Alves <118781681+thacioalves@users.noreply.github.com>
2023-03-30 19:48:26 -0300

    Footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
 import "./Video.css";
 
 function Video() {
@@ -24,6 +25,7 @@ function Video() {
         loop
         src="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"
       ></video>
+      <VideoFooter/>
     </div>
   );
 }
